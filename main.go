01e6c8c5dcd735d6d5dcc6d076abe136aadfde0b
Sean Blevins <seanbinvt@users.noreply.github.com>
2020-07-30 18:29:48 -0400

    import fixes

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("ATLAS_URI")))
 
 	if err != nil {
 		log.Fatal(err)
