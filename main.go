56fb45b4ed87a31ec537ebd56642209d7806687c
Sean Blevins <seanbinvt@users.noreply.github.com>
2020-08-10 00:33:08 -0400

    Enable CORS test

--- main.go
+++ main.go
@@ -190,5 +190,6 @@ func submitBattleReport(w http.ResponseWriter, r *http.Request) {
 
 func allowOpts(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
